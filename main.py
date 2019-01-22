9d33eceec2d18a5621e114eb35a82559dc52a5ea
Leonard Lorenz <contact@metr1xx.de>
2019-01-22 22:45:23 +0100

    rainbow now starts with red. i like red.

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def rgb_to_colour(r, g, b):
 def calc_colour(i):
     i = i * 0.1
     brightness = 1
-    red = brightness * (math.sin(i + 2) + 1) * 128;
-    green = brightness * (math.sin(i + 4) + 1) * 128;
-    blue = brightness * (math.sin(i + 6) + 1) * 128;
+    red = brightness * (math.sin(i + 1.5) + 1) * 128;
+    green = brightness * (math.sin(i + 3.5) + 1) * 128;
+    blue = brightness * (math.sin(i + 5.5) + 1) * 128;
     #print(str(red) + ' ' + str(green) + ' ' + str(blue))
     return rgb_to_colour(int(red), int(green), int(blue))
 
@@ -40,22 +40,14 @@ def main():
         input_image = sys.argv[1]
         fuzz_factor = 50000
         colour_to_replace = 'white'
-        output_path = ''
 
         if sys.argv[2]:
             fuzz_factor = sys.argv[2]
         if sys.argv[3]:
             colour_to_replace = sys.argv[3]
-        if len(sys.argv) > 4:
-            output_path = sys.argv[4].split('.')[0] + "-converted." + input_image.split('.')[1]
-        else:
-            output_path = input_image.split('.')[0] + "-converted." + input_image.split('.')[1]
-
-        if output_path is '':
-            print('No output path specified. Exitting.')
-            help()
 
         for i in range(0, 60):
+            output_path = os.getcwd() + input_image.split('.')[-2] + '-converted-' + str(i) + '.png'
 
             output_colour = calc_colour(i)
             print(output_colour)
@@ -63,9 +55,7 @@ def main():
             convert_path = os.popen('which convert').read().strip('\n')
             print(convert_path)
 
-            final_output_path = output_path.split('.')[0] + '-' + str(i) + '.' + output_path.split('.')[1]
-
-            subprocess.call([convert_path, input_image, '-fill', output_colour, '-fuzz', fuzz_factor, '-opaque', colour_to_replace, final_output_path])
+            subprocess.call([convert_path, input_image, '-fill', output_colour, '-fuzz', fuzz_factor, '-opaque', colour_to_replace, output_path])
     else:
         help()
 
