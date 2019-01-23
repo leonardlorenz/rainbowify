efb1c308a71cf7657a4553a8ba4925835e468234
Leonard Lorenz <contact@metr1xx.de>
2019-01-23 01:36:10 +0100

    optimized bandwidth usage for gifs a bit

--- src/imgtogif.sh
+++ src/imgtogif.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # first launch option should be the input files (e.g. /path/to/directory/*.png
 # second option is the output file
-convert -delay 10 -loop 0 $1 $2
+convert -delay 10 -layers optimize -loop 0 $1 $2
